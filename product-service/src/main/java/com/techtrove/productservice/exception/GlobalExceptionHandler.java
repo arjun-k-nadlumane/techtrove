361d7360a7fbefe60f8f141206e4b56b4ac89dfb
aditisakpal <adisakpal14@gmail.com>
2025-05-07 13:33:52 +0530

    all working

--- product-service/src/main/java/com/techtrove/productservice/exception/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/techtrove/productservice/exception/GlobalExceptionHandler.java
@@ -5,10 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
+// @Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     // Handle validation errors
